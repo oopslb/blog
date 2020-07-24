cf0192ef5a39a1b7fde638ad2980d694584380be
oopslb <1923561987@qq.com>
2020-07-24 19:08:09 +0800

    '数据库配置'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'Host': '127.0.0.1',
+        'Port': 3306,
+        'USER': 'oops',
+        'PASSWORD': '123123',
+        'NAME': 'blog',
     }
 }
 
